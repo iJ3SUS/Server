150987cc96c25c2c2feb24b6c3187150c835b823
Jesus Rueda <jesus.r@bellpi.com>
2023-02-17 16:51:19 -0500

    DELETE PROYECT

--- docker/test/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-service ssh start
-
-#cd /var/www/html && pm2 --name exegol-biz --error /home/exegol/logs/exegol-biz-error.log start npm -- start
-
-nginx -g "daemon off;"
--- plugins/env.js
+++
@@ -1,5 +0,0 @@
-import * as dotenv from 'dotenv'
-
-const env = dotenv.config().parsed
-
-export default env
